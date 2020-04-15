261a25efde130a63e1cb21abdea5a6904d04ca8b
jakem <Hondazx123>
2020-04-16 00:28:39 +0100

    post and get request to DB working

--- backend/app.js
+++ backend/app.js
@@ -48,26 +48,30 @@ app.post("/api/posts", (req, res, next) => {
 
 });
 
-app.use('/api/posts',(req,res, next)=>{
-  const posts = [
-    {
-    id:'hgvhjggvvbh',
-    title:'first server side post',
-    content: 'this is coming from the server'
-  },
-  {
-    id:'gvkgjgkbhkjbhhbk',
-    title:'first server side post',
-    content: 'this is coming from the server'
+app.get('/api/posts',(req,res, next)=>{
+  // const posts = [
+  //   {
+  //   id:'hgvhjggvvbh',
+  //   title:'first server side post',
+  //   content: 'this is coming from the server'
+  // },
+  // {
+  //   id:'gvkgjgkbhkjbhhbk',
+  //   title:'first server side post',
+  //   content: 'this is coming from the server'
+  //
+  // }
+  // ];
 
-  }
-  ];
-
-
-  res.status(200).json({
-    message:'Posts fetched succesfully',
-    posts: posts
+  Post.find().then(documents => {
+    console.log(documents);
+    res.status(200).json({
+      message:'Posts fetched succesfully',
+      posts: documents
+    });
   });
+
+
 });
 
 module.exports = app;
