745fb663c700f8fe283795935d724c83ea4e8381
jakem <Hondazx123>
2020-04-17 18:21:13 +0100

    updateding post server side

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -37,8 +37,7 @@ export class PostsService {
   }
 
   getPost(id: string){
-    return {...this.posts.find(p => p.id === id)
-    };
+    return this.http.get<{_id: string; title: string; content: string}>('http://localhost:3000/api/posts/' + id);
   }
 
 
@@ -63,6 +62,12 @@ export class PostsService {
     const post: Post = {id, title, content};
     this.http.put('http://localhost:3000/api/posts/' + id, post)
       .subscribe(response => console.log(response));
+
+    const  updatedPosts = [...this.posts];
+    const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+    updatedPosts[oldPostIndex] = post;
+    this.posts = updatedPosts;
+    this.postsUpdated.next([...this.posts]);
   }
 
   deletePost(postId: string){
