179fdc8dd9e6cde3cf47277d92258e9a87f589da
jakem <Hondazx123>
2020-04-26 13:47:30 +0100

    adding error interceptor to handle http errrors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {LoginComponent} from './auth/login/login.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {AuthInterceptor} from './auth/auth-interceptor';
-
+import {ErrorInterceptor} from './error-interceptor';
 
 
 @NgModule({
@@ -51,7 +51,10 @@ import {AuthInterceptor} from './auth/auth-interceptor';
     HttpClientModule,
     MatPaginatorModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
